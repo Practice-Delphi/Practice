fbe0f7e23123d83599fc515e2208d49690cecdd0
Roman Dobera <32237800+uigur5@users.noreply.github.com>
2018-06-10 14:11:49 +0300

    Add files via upload

--- backend/identity/Models/User.cs
+++ backend/identity/Models/User.cs
@@ -5,8 +5,7 @@ namespace identity.Models
     public class User : IdentityUser
     {
         public int Year { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+
 
     }
 }
